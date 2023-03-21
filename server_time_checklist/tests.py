d0a999cde5c11871bb3c3073581c30ba92dd4d78
helen <1036808976@qq.com>
2023-03-21 16:55:57 +0800

    2023-03-21

--- server_time_checklist/tests.py
+++ server_time_checklist/tests.py
@@ -3,9 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 from datetime import datetime
 
-date_str = "2023-03-21"
-date_obj = datetime.strptime(date_str, "%Y-%m-%d")
-day_value = int(date_str.split('-')[2])
-
-print(day_value)
-print(type(day_value))
+sqlCheck_1 = "select * from server_time_checklist_data"
+print(sqlCheck_1)
