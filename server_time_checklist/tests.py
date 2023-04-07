ef7cc52df4f9185cb59496718d2b8e28a1999dcb
helen <1036808976@qq.com>
2023-04-07 17:18:20 +0800

    2023-04-07 修改checklist_page中数据用完的情况

--- server_time_checklist/tests.py
+++ server_time_checklist/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 from datetime import datetime
 
-sqlCheck_1 = "select * from server_time_checklist_data"
-print(sqlCheck_1)
+# sqlCheck_1 = "select * from server_time_checklist_data"
+print(296//6)
--- server_time_checklist/views.py
+++ server_time_checklist/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Max
+from django.db.models import Max, Min
 from django.db.models.expressions import Random
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -24,31 +24,48 @@ def checklist_page(request, selected_time):
     selected_time_obj = datetime.strptime(selected_time, '%Y-%m-%d')
     start_id = 1
     end_id = ServerTimeChecklist.objects.aggregate(Max('id'))['id__max']
+
+    yesterday_start = selected_time_obj - timedelta(days=1)
+    yesterday_end = yesterday_start + timedelta(days=1, seconds=-1)
+
+    # 每日服务器检查量
     desired_object_count = 15
-    queryset = ServerTimeChecklist.objects.filter(id__range=(start_id, end_id), time_synced=0,
-                                                  review_time__in=[selected_time_obj - timedelta(days=1)]).order_by(
-        'id')[:desired_object_count]
+
+    queryset = ServerTimeChecklist.objects.filter(id__range=(start_id, end_id), time_synced=0).order_by('id')[
+               :desired_object_count]
+
+    # review_time__in = [selected_time_obj - timedelta(days=1)]
     # print(queryset.count())
+    first_record_id = ServerTimeChecklist.objects.filter(check_time__range=(yesterday_start, yesterday_end)).order_by(
+        'check_time').aggregate(Min('id'))['id__min']
+
+    last_record_id = ServerTimeChecklist.objects.filter(check_time__range=(yesterday_start, yesterday_end)).order_by(
+        '-check_time').aggregate(Max('id'))['id__max']
+    if last_record_id - first_record_id > desired_object_count:
+        records = ServerTimeChecklist.objects.filter(id__range=(first_record_id, first_record_id + 15),
+                                                     check_time__range=(yesterday_start, yesterday_end))
+        last_record_id = records.aggregate(Max('id'))['id__max']
+
     if queryset.count() < desired_object_count:
         ids = queryset.values_list('id', flat=True)
-        random_queryset = ServerTimeChecklist.objects.filter(time_synced__isnull=True).order_by('id')[
-                          :desired_object_count - len(ids)]
+        restlen = desired_object_count - len(ids)
+
+        random_queryset = ServerTimeChecklist.objects.filter(id__gt=last_record_id).order_by('id')[:restlen]
         queryset = list(queryset) + list(random_queryset)
-    queryset = sorted(queryset, key=lambda x: x.id)
-    queryset = list(queryset)
-    # print(len(queryset))
-    # print(selected_time_obj.day)
-    # for data in queryset:
-    #     check_time_obj = datetime.strptime(data.check_time, '%Y-%m-%d')
-    #     days_difference = selected_time_obj.day - check_time_obj.day
-    #     print(days_difference)
-    #     if days_difference >= 2 and data.time_synced == 0:
-    #         data.is_red= True
-    #     else:
-    #         data.is_red = False
-    #     data.save()
+
+    # queryset = sorted(queryset, key=lambda x: x.id)
+    finalset = list(queryset)
+
+    # 数据库最后数据行的操作
+    if len(finalset) < desired_object_count:
+        random_queryset = ServerTimeChecklist.objects.filter(id__gt=0).order_by('id')[
+                          :desired_object_count - len(finalset)]
+        finalset = list(random_queryset) + list(finalset)
+
+    finalset = list(finalset)
+
     return render(request, 'updatechecklist.html',
-                  {'selected_time': selected_time, 'queryset': queryset, 'date_today': selected_time_obj})
+                  {'selected_time': selected_time, 'queryset': finalset, 'date_today': selected_time_obj})
 
 
 @require_POST
@@ -58,7 +75,9 @@ def update_checklist(request):
         # print(request.method)
         # print(request.POST.getlist('forms'))
         form_data = request.POST.getlist('forms')
-        for i in range(len(form_data) // 6):
+        data_len = len(form_data) // 6
+        for i in range(data_len):
+
             # 解析表单数据
             id = form_data[i * 6 + 0]
             time_synced = form_data[i * 6 + 1]
@@ -66,6 +85,9 @@ def update_checklist(request):
             review_time = form_data[i * 6 + 3]
             is_adjusted = form_data[i * 6 + 4]
             checked_by = form_data[i * 6 + 5]
+
+            # if time_synced == 0:
+            #     review_time = datetime.now().date() - timedelta(days=1)
 
             # print(review_time)
             # 根据ID查找对应的ServerTimeChecklist对象
@@ -116,7 +138,7 @@ def update_checklist(request):
             #         'is_adjusted': server_time_checklist.is_adjusted,
             #         'checked_by': server_time_checklist.checked_by,
             #     })
-            if i == len(form_data) // 6:
+            if i == data_len:
                 break
     # 渲染模板，并传递checklist_data到模板中
     return render(request, 'checklist.html', {'checklist_data': checklist_data})
